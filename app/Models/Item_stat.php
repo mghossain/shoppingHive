fcabcf8ecc837d7fd94d14730d407cf357acd5f4
mike <mghossain.42@gmail.com>
2023-03-14 11:07:18 +0200

    add product - item stats relation

--- app/Models/Item_stat.php
+++ app/Models/Item_stat.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Item_stat extends Model
 {
     protected $fillable = ['product_id', 'addedCount', 'removedCount', 'purchasedCount'];
+
+    protected $with = ['product'];
+
+    public function product()
+    {
+        return $this->belongsTo('App\Models\Product');
+    }
 }
